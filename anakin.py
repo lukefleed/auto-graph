08d97c2de2b59bde751ca18482dcb0446792495b
Luca Lombardo <l.lombardo@protonmail.com>
2021-11-21 23:37:47 +0100

    aggiunto all

--- anakin.py
+++ anakin.py
@@ -3,47 +3,65 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-test = input("Quali test vuoi guardare?:\n [1] CPU\n [2] GPU\n [3] Giochi\n Scrivi il numero: ")
+# test = input("Quali test vuoi guardare?:\n [1] CPU\n [2] GPU\n [3] Giochi\n Scrivi il numero: ")
 
-xl = pd.ExcelFile('bench/cpu.xlsx')
+EXCEL_FILE = 'bench/cpu.xlsx'
 
-categoria = input("\nScegli uno dei seguenti benchmark\n  "
-                  + "\n  ".join(
-                      f"[{i}] {n}" for i, n in enumerate(xl.sheet_names))
-                  + "\nScrivi il numero: ")
+xl = pd.ExcelFile(EXCEL_FILE)
 
-excel_file = 'bench/cpu.xlsx'
-benchmark = pd.read_excel(excel_file, sheet_name=int(categoria))
-
-cpus = input(
-    "\nScegli quali processori confrontare\n  "
+categorie = input(
+    "\nScegli uno dei seguenti benchmark\n  "
     + "\n  ".join(
-        f"[{i}] {n}" for i, n in enumerate(benchmark['CPU']))
-    + "\nScrivi i numeri separati da spazi: ").split()
-cpus = [int(x) for x in cpus]
-
-# data = {'multicore': benchmark['Multi Core'][cpus].to_list(),
-#         'singlecore': benchmark['Single Core'][cpus].to_list()
-#        }
-data = {col: benchmark[col][cpus].to_list() for col in benchmark.columns[1:]}
-
-df = pd.DataFrame(
-    data,
-    #columns=['multicore','singlecore'],
-    index=benchmark['CPU'][cpus].to_list())
+        f"[{i+1}] {n}" for i, n in enumerate(xl.sheet_names[1:]))
+    + "\n  [lascia vuoto] Tutti i benchmark"
+    + "\nScrivi il numero: ")
+if categorie:
+    categorie = [int(categorie)]
+else:
+    categorie = list(range(1, len(xl.sheet_names)))
 
 plt.style.use('ggplot')
-df.plot.barh(color=["#303030", "#F39200"])
+for categoria in categorie:
+    benchmark = pd.read_excel(EXCEL_FILE, sheet_name=int(categoria))
 
+    if len(categorie) == 1:
+        cpus = input(
+            "\nScegli quali processori confrontare\n  "
+            + "\n  ".join(
+                f"[{i}] {n}" for i, n in enumerate(benchmark['CPU']))
+            + "\n  [lascia vuoto] Tutti i processori"
+            + "\nScrivi i numeri separati da spazi: ").split()
+        cpus = [int(x) for x in cpus]
+    else:
+        cpus = False
+    if not cpus:
+        cpus = list(range(len(benchmark)))
 
-titolo = input("\nTitolo del grafico: ")
-plt.title(titolo)
+    # data = {'multicore': benchmark['Multi Core'][cpus].to_list(),
+    #         'singlecore': benchmark['Single Core'][cpus].to_list()
+    #        }
+    data = {col: benchmark[col][cpus].to_list() for col in benchmark.columns[1:]}
 
-ordinate = input("Titolo asse Y: ")
-plt.ylabel(ordinate)
+    plt.rc("font", size=22)
+    df = pd.DataFrame(
+        data,
+        #columns=['multicore','singlecore'],
+        index=benchmark['CPU'][cpus].to_list())
 
-ascisse = input("Titolo asse X: ")
-plt.xlabel(ascisse)
+    #plt.figure()  # Ci pensa barh
+    df.plot.barh(color=["#F39200", "#303030"]) 
 
-plt.show()
+    titoli = pd.read_excel(EXCEL_FILE, sheet_name=0)
+    plt.gcf().set_size_inches(38.4, 19.2)
+    #titolo = input("\nTitolo del grafico: ")
+    plt.suptitle(titoli["Titolo"][int(categoria)-1])
+    plt.title(titoli["Sottotitolo"][int(categoria)-1])
+    #ordinate = input("Titolo asse Y: ")
+    plt.ylabel(titoli["Y"][int(categoria)-1])
+    #ascisse = input("Titolo asse X: ")
+    plt.xlabel(titoli["X"][int(categoria)-1])
+    
+    plt.savefig(f"foto/{titoli['Scheda'][int(categoria)-1]}.png", dpi=100)
+
+#plt.show()
 
