b1e4782418ebdc8055dd663b3ac96fb8be015cad
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2021-12-14 16:20:45 +0100

    refactoring part 1 (#1)
    * added requirements
    
    * output class
    
    * clean up and empty string
    
    * removed numpy and added old code back for legacy purposes
    
    * sample picture
    
    * pylint

--- anakin.py
+++ anakin.py
@@ -1,68 +1,63 @@
-#!/usr/bin/env python3
+'''main program'''
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+from output import Output
 
-test = input("Quali test vuoi guardare?:\n [1] CPU\n [2] GPU\n [3] Games\n Enter the number: ")
-if test == '1':
-    EXCEL_FILE = 'bench/cpu.xlsx'
-elif test == '2':
-    EXCEL_FILE = 'bench/gpu.xlsx'
 
-#EXCEL_FILE = 'bench/cpu.xlsx'
+def main():
+    '''main function'''
+    out = Output()
+    tests = ["CPU", "GPU", "GIOCHI"]
+    test_input = out.print_and_single_selection("Quali test vuoi guardare?", tests)
 
-xl = pd.ExcelFile(EXCEL_FILE)
+    excel_file = "bench/cpu.xlsx" if test_input == 1 else "bench/gpu.xlsx"
+    xl_dataframe = pd.ExcelFile(excel_file)
 
-categorie = input(
-    "\nChoose one of the following benchmarks\n  "
-    + "\n  ".join(
-        f"[{i+1}] {n}" for i, n in enumerate(xl.sheet_names[1:]))
-    + "\n  [press enter] All benchmarks"
-    + "\nWrite the number: ")
-if categorie:
-    categorie = [int(categorie)]
-else:
-    categorie = list(range(1, len(xl.sheet_names)))
+    categorie = out.print_and_multi_selection(
+        "Digita i benchmark che vuoi graficare, separati da virgola",
+        xl_dataframe.sheet_names[1:],
+    )
+    print(categorie)
 
-plt.style.use('ggplot')
-for categoria in categorie:
-    benchmark = pd.read_excel(EXCEL_FILE, sheet_name=int(categoria))
+    # TODO refactor
+    plt.style.use("ggplot")
+    for categoria in categorie:
+        benchmark = pd.read_excel(excel_file, sheet_name=int(categoria))
 
-    if len(categorie) == 1:
-        cpus = input(
-            "\nChoose which products compare\n  "
-            + "\n  ".join(
-                f"[{i}] {n}" for i, n in enumerate(benchmark['CPU']))
-            + "\n  [press enter] All products"
-            + "\nWrite the numbers divided by spaces: ").split()
-        cpus = [int(x) for x in cpus]
-    else:
-        cpus = False
-    if not cpus:
-        cpus = list(range(len(benchmark)))
+        if len(categorie) == 1:
+            cpus = input(
+                "\nChoose which products compare\n  "
+                + "\n  ".join(f"[{i}] {n}" for i, n in enumerate(benchmark["CPU"]))
+                + "\n  [press enter] All products"
+                + "\nWrite the numbers divided by spaces: "
+            ).split()
+            cpus = [int(x) for x in cpus]
+        else:
+            cpus = False
+        if not cpus:
+            cpus = list(range(len(benchmark)))
 
-    # data = {'multicore': benchmark['Multi Core'][cpus].to_list(),
-    #         'singlecore': benchmark['Single Core'][cpus].to_list()
-    #        }
-    data = {col: benchmark[col][cpus].to_list() for col in benchmark.columns[1:]}
+        # data = {'multicore': benchmark['Multi Core'][cpus].to_list(),
+        #         'singlecore': benchmark['Single Core'][cpus].to_list()
+        #        }
+        data = {col: benchmark[col][cpus].to_list() for col in benchmark.columns[1:]}
 
-    plt.rc("font", size=24)
-    df = pd.DataFrame(
-        data,
-        index=benchmark['CPU'][cpus].to_list())
+        plt.rc("font", size=24)
+        dataframe = pd.DataFrame(data, index=benchmark["CPU"][cpus].to_list())
 
-    #plt.figure()  # Ci pensa barh
-    df.plot.barh(color=["#F39200", "#303030"])
+        # plt.figure()  # Ci pensa barh
+        dataframe.plot.barh(color=["#F39200", "#303030"])
 
-    titoli = pd.read_excel(EXCEL_FILE, sheet_name=0)
-    plt.gcf().set_size_inches(38.4, 19.2)
+        titoli = pd.read_excel(excel_file, sheet_name=0)
+        plt.gcf().set_size_inches(38.4, 19.2)
 
-    plt.suptitle(titoli["Titolo"][int(categoria)-1])
-    plt.title(titoli["Sottotitolo"][int(categoria)-1])
-    plt.ylabel(titoli["Y"][int(categoria)-1])
-    plt.xlabel(titoli["X"][int(categoria)-1])
+        plt.suptitle(titoli["Titolo"][int(categoria) - 1])
+        plt.title(titoli["Sottotitolo"][int(categoria) - 1])
+        plt.ylabel(titoli["Y"][int(categoria) - 1])
+        plt.xlabel(titoli["X"][int(categoria) - 1])
 
-    plt.savefig(f"foto/{titoli['Scheda'][int(categoria)-1]}.png", dpi=100)
+        plt.savefig(f"foto/{titoli['Scheda'][int(categoria)-1]}.png", dpi=100)
 
-#plt.show()
 
+if __name__ == "__main__":
+    main()
---
+++ output.py
@@ -0,0 +1,65 @@
+import os
+class Output:
+
+    def __init__(self) -> None:
+        self.clear_console()
+
+    @staticmethod
+    def clear_console():
+        '''clears the console'''
+        os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+
+    @staticmethod
+    def print(str: str=""):
+        '''prints the passed string'''
+        print(str)
+
+    def print_red(self, str: str=''):
+        '''prints the passed string in red'''
+        self.print(f"\033[91m{str}\033[00m")
+
+    @staticmethod
+    def print_numbered_list(elements: list, start: int=1):
+        '''prints a list of elements by line'''
+        for counter, element in enumerate(elements, start=start):
+            print(f"{counter}) {element}")
+
+    def print_and_selection_input(self, title: str, elements:list, start: int=1) -> str:
+        '''prints a title, a numbered list and waits for user input'''
+        self.print(title)
+        self.print_numbered_list(elements, start)
+        return input()
+
+    def print_and_single_selection(self, title: str, elements:list, start: int=1) -> int:
+        '''prints a title, a numbered list, waits for user input 
+            and returns the input number'''
+        while True:
+            user_input = self.print_and_selection_input(title, elements, start)
+
+            try:
+                user_input = int(user_input)
+                if 0 < user_input <= len(elements):
+                    return user_input
+                raise ValueError
+
+            except ValueError:
+                self.clear_console()
+                self.print_red('Input non valido, riprova')
+
+    def print_and_multi_selection(self, title: str, elements:list, start: int=1) -> list:
+        '''prints a title, a numbered list, waits for user input 
+            and returns the input numbers. If the user input is none, everthing will be selected'''
+        while True:
+            user_input = self.print_and_selection_input(title, elements, start)
+
+            try:
+                if user_input == '': return [counter for counter, _ in enumerate(elements, start=start)]
+                inputs = [int(element.strip()) for element in user_input.split(',')]
+                if all(x<=len(elements) and x>0 for x in inputs):
+                    return inputs
+                raise ValueError
+
+            except ValueError:
+                self.clear_console()
+                self.print_red('Input non valido, riprova')
+    
