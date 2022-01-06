177cb35964c11ea56ebef586513179985c041579
Sclafus <alessandro.sclafani00@gmail.com>
2022-01-06 18:25:15 +0100

    plotting class

--- anakin.py
+++ anakin.py
@@ -1,9 +1,8 @@
 '''main program'''
-import matplotlib.pyplot as plt
 import pandas as pd
 from output import Output
 from tqdm import tqdm
-import pathlib
+from plotting import Plot
 
 ARANCIONE = '#F39200'
 GRIGIO_SCURO = '#303030'
@@ -23,37 +22,28 @@ def main():
         xl_dataframe.sheet_names[1:],
     )
 
-    pathlib.Path('foto').mkdir(parents=True, exist_ok=True)
-
     excel_data = pd.read_excel(excel_file, sheet_name=categorie)
     titoli = pd.read_excel(excel_file, sheet_name=0)
 
     for bench in tqdm(excel_data):
         df = excel_data[bench]
-        bench_name = xl_dataframe.sheet_names[bench]
 
-        benchs = {
-            tipologia: list(df[tipologia].sort_values().values) for tipologia in df[df.columns[1:]]
-        }
-        df = pd.DataFrame(benchs, index=df[df.columns[0]].values)
+        benchs = pd.DataFrame(
+            {
+                tipologia: list(df[tipologia].sort_values().values)
+                for tipologia in df[df.columns[1:]]
+            },
+            index=df[df.columns[0]].values,
+        )
 
-        # plotting
-        ax = df.plot.barh(color=[ARANCIONE, GRIGIO_SCURO])
-
-        # numeri davanti alle barre
-        for container in ax.containers:
-            ax.bar_label(container)
-
-
-        plt.style.use('ggplot')
-        plt.gcf().set_size_inches(38.4, 19.2)
-
-        plt.suptitle(titoli["Titolo"][bench - 1])
-        plt.title(titoli["Sottotitolo"][bench - 1])
-        plt.xlabel(titoli["X"][bench - 1])
-        plt.ylabel(titoli["Y"][bench - 1])
-
-        plt.savefig(pathlib.Path('foto') / bench_name, dpi=100)
+        plt = Plot([ARANCIONE, GRIGIO_SCURO])
+        plt.plot_barh(
+            benchs,
+            titoli["Titolo"][bench - 1],
+            titoli["Sottotitolo"][bench - 1],
+            titoli["X"][bench - 1],
+            titoli["Y"][bench - 1]
+        )
 
 
 if __name__ == '__main__':
