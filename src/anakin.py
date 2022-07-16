b079817561e6c53042f57af03734928c87766af6
Francesco Boschi <francesco.boschi2@studio.unibo.it>
2022-07-16 14:31:36 +0200

    implemented argparse

--- src/anakin.py
+++ src/anakin.py
@@ -18,40 +18,47 @@ def main():
     '''main function'''
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-file", help="Inserisci il file da convertire in grafico")
-    parser.add_argument("-sheets", help="Inserisci i fogli da convertire in grafico, separati da virgola, 'all' per convertirli tutti")
+    parser.add_argument("-file", help="Inserisci il nome file da convertire in grafico", required=True)
+    parser.add_argument("-sheets", help="Inserisci i fogli da convertire in grafico, separati da virgola, 'all' per convertirli tutti",required=True)
+    
+    args = parser.parse_args()
 
+    file_name = args.file
+    sheets = args.sheets
     out = Output()
 
     # Create the input folder if it does not exist
     Path(INPUT_DIR).mkdir(parents=True, exist_ok=True)
-    excel_files = [excel_file for excel_file in Path(INPUT_DIR).glob('**/*') if excel_file.is_file()]
+    
+    file_path = Path(os.path.join(INPUT_DIR,file_name))
+    print(file_path)
+    if(not os.path.exists(file_path)):
+        print("Inserire un file valido")
+        exit()
 
-    if not excel_files:
-        out.print('La cartella "input" non contiene file Excel', Output.Color.RED)
-        sys.exit(1)
+    excel_file = pd.ExcelFile(file_path)
 
-    file_index = out.print_and_select('Seleziona il file', excel_files)
-    out.clear()
+    if sheets == "all":
+        sheets = None
+    else:
+        sheets = sheets.split(",")
+        sheets = [int(i) for i in sheets]
+        print(sheets)
+    if sheets != None:
+        for elem in sheets:
+            if elem > len(excel_file.sheet_names) or elem < 0:
+                print("Inserire indici validi")
+                exit()
 
-    excel_file_path = excel_files[file_index]
-    excel_file = pd.ExcelFile(excel_file_path)
+    excel_data = pd.read_excel(excel_file, sheet_name=sheets, header=None, index_col=None)
 
-    sheets_indexes = out.print_and_select(
-        'Inserisci i fogli da convertire in grafico, separati da virgola [ENTER per selezionarli tutti]',
-        excel_file.sheet_names,
-        multi=True
-    )
-    out.clear()
-
-    excel_data = pd.read_excel(excel_file_path, sheet_name=sheets_indexes, header=None, index_col=None)
-
-    output_dir = Path(OUTPUT_DIR).joinpath(os.path.splitext(excel_file_path)[0].split(os.path.sep)[1])
+    output_dir = Path(OUTPUT_DIR).joinpath(os.path.splitext(file_path)[0].split(os.path.sep)[1])
     plt = Plot(output_dir)
 
     out.print('Conversione in corso...\n')
 
     for sheet in tqdm(excel_data):
+        print(sheet)
         plt.plot_graph(excel_data[sheet], excel_file.sheet_names[sheet])
 
     out.print(f'\nImmagini salvate in "{output_dir}".', Output.Color.GREEN)
